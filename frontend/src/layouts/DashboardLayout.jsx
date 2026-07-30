f66240f5eb57487f6ba07559ca5940fbf509354a
Maria Ezcurra <mezcurra@unsa.edu.pe>
2026-07-29 23:23:56 -0500

    update 2

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
+import Header from "../Components/Header";
+import Sidebar from "../Components/Sidebar";
 import Usuarios from "../Pages/Usuarios";
 //TI
 import UploadLectura from "../Pages/TI/UploadLectura";  
